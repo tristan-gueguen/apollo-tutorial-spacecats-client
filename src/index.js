a78ef65735de4cdfc649c4f8939d30c7370cd9db
tristan-gueguen <tristan.gueguen@gmail.com>
2021-09-17 23:38:17 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import GlobalStyles from "./styles";
+import Pages from "./pages";
+
+const client = new ApolloClient({
+  uri: "https://tutorial-apollo-spacecats.herokuapp.com/",
+  cache: new InMemoryCache(),
+});
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ApolloProvider client={client}>
+      <GlobalStyles />
+      <Pages />
+    </ApolloProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
